4f6b255a519f842414b47083d2e8e339d1153d77
alexgulliver <alex.gulliver6@gmail.com>
2025-01-05 23:05:46 +0000

    initial commit

---
+++ CognitiveMap/NodeClass.py
@@ -0,0 +1,12 @@
+import networkx as nx
+
+G = nx.Graph()
+
+class Node:
+    def __init__(self, resource_availability, resource_replenishment, agent_density, behavioural_state):
+        self.resource_availability = resource_availability
+        self.resource_replenishment = resource_replenishment
+        self.agent_density = agent_density
+        self.behavioural_state = behavioural_state
+
+        
